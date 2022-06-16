c41a9ffee899e86e6d092ff91ebc6404df2f349f
JuliDeveloper <rja-97@yandex.ru>
2022-06-16 16:05:13 +0300

    Fixed code

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -5,15 +5,15 @@
 //  Created by Julia Romanenko on 16.06.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var viewAnimate: UIView!
+    @IBOutlet weak var viewAnimate: SpringView!
     @IBOutlet weak var discriptionLable: UILabel!
     
     
-    @IBAction func enableAnimation(_ sender: UIButton) {
+    @IBAction func enableAnimation(_ sender: SpringButton) {
     }
 }
 
